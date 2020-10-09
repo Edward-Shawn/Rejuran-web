5776903ae4c0815b102c375f0f620ffc0f96ccd5
Edward-Shawn <sbh3834@naver.com>
2020-10-09 23:30:40 +0900

    포폴용 모바일 웹 제작 시작

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    
+    var didScroll;
+
+    $(window).scroll(function(){
+        didScroll = true;
+        if(didScroll){
+            hasScrolled();
+            didScroll = false;
+        }
+    })
+
+
+
+    // setInterval(function(){
+    //     if(didScroll){
+    //         hasScrolled();
+    //         didScroll = false;
+    //     }
+        
+    // },100);
+    
+    function hasScrolled(){
+
+        var windowWit = $(window).scrollTop();
+
+        if(windowWit == 0){
+            $("header").removeClass("on");
+        }else{
+            $("header").addClass("on");
+        }
+    }
+})
